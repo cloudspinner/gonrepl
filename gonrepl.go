c550a3feb40a77beaa28a0bc7ca0a6ab338c1b49
Eugene Ma <ema@twitter.com>
2015-03-10 23:47:20 -0700

    Wording.

--- gonrepl.go
+++ gonrepl.go
@@ -1,7 +1,7 @@
 // An nREPL client that reads Clojure code from standard input and
 // writes the result to standard output.  Connects to
-// localhost:$LEIN_REPL_PORT by default. -a flag overrides the default
-// address.
+// localhost:$LEIN_REPL_PORT by default. Pass the -a flag to override
+// the default address.
 package main
 
 import (
